d097d5396a5e1a33aff3133a38124bf6499a732a
Baumhardt <baumhana@vanderbilt.edu>
2019-04-25 11:25:44 -0500

    major changes

--- IPTrace.py
+++ IPTrace.py
@@ -1,21 +1,8 @@
-##############################################################################
-# VUMC Network Endpoint Locator
-# -----------------------------
-#
-# Notes:
-# Cisco Prime integration using the GET Devices API
-# documentation found at https://10.109.24.16/webacs/api/v4/data/Devices?_docs
-###
-#To Do
-# IP validation
-# determine switch platform/version for proper commands
-##############################################################################
-
 import paramiko
 from getpass import getpass
 import re
 import sys
-from network_functions import traceroute
+import network_functions
 import subprocess
 import os
 import pprint
@@ -23,7 +10,7 @@ import pprint
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 mac_pattern = re.compile('([0-9a-fA-F]{4}.[0-9a-fA-F]{4}.[0-9a-fA-F]{4})')
-int_pattern = re.compile('([a-zA-Z]{1,2}[0-9]{1}/[0-9]{1,2}/?[0-9]{1,2})')
+int_pattern = re.compile('([a-zA-Z]{1,3}[0-9]{1}/[0-9]{1,2}/?[0-9]{1,2})')
 po_int_pattern = re.compile('([a-zA-Z]){1,2}[0-9]{1,4}')
 ip_pattern = re.compile('IP address: ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})')
 subnet_pattern = re.compile('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2})')
@@ -40,144 +27,57 @@ snmp_command = 'show snmp location'
 iproute_cmd = 'show ip route '
 cdp_neighbors = 0
 
-def find_gateway(ip_trace, root_ip):
-
-    print ('Beginning ip route trace to locate gateway...')
-
-    print ('Contacting device: ' + root_ip + ' to look for routes to ' + ip_trace + '...')
-    next_hop = get_next_hop(ip_trace, root_ip)
-    while True:
-        print ('contacting the next-hop: ' + next_hop)
-        hop_ip = next_hop
-        next_hop = get_next_hop(ip_trace, next_hop)
-        if next_hop == ip_only_pattern.search(next_hop):
-            continue
-        elif next_hop == 'connected':
-            response = ssh_login(hop_ip, iproute_cmd, ip_trace)
-            ssh_output = response.read()
-            raw_output = ssh_output.decode(encoding='utf-8')
-            connected = parse(raw_output, connected_pattern)
-            route = parse(raw_output, next_hop_pattern)
-            static = parse(raw_output, static_route_pattern)
-
-            match = connected_pattern.search(raw_output)
-            vlan_id = match.group(1)
-            svi = "vlan" + vlan_id
-            response = ssh_login(hop_ip, show_run_int_cmd, svi)
-            svi_ip_config = parse(response, ip_config_pattern)
-            gateway_ip = parse(svi_ip_config, ip_only_pattern)
-            print (gateway_ip)
-            return gateway_ip
-
-        else:
-            return
-
-# function to get log in to a network device and get the next hop for a route.
-def get_next_hop(ip_trace, hop_ip):
-    #log in to current hop and return show ip route output
-    print ('Looking for next hop to the Gateway IP for ' + ip_trace + ' .....')
-    response = ssh_login(hop_ip, iproute_cmd, ip_trace)
-    ssh_output = response.read()
-    raw_output = ssh_output.decode(encoding='utf-8')
-    connected = parse(raw_output, connected_pattern)
-    route = parse(raw_output, next_hop_pattern)
-    static = parse(raw_output, static_route_pattern)
-
-    print ('\n ')
-    print ('--------------------------')
-    print ('Route Results Summary: ')
-    print ('--------------------------\n')
-    print ('Static: ' + static)
-    print ('Route: ' + route)
-    print ('Connected: ' + connected)
-    print ('--------------------------\n')
-
-    if static != "Not Found":
-        print ('-----------------------------------------')
-        print ("Found a static next-hop: " + route + '\n')
-        print ('-----------------------------------------')
-        #extract just the IP from the route
-        match = ip_only_pattern.search(route)
-        if match:
-            next_hop_ip = match.group(0)
-            # gather arp information to identify use for outgoing interface identification
-            response = ssh_login(hop_ip, arp_command, next_hop_ip)
-            arp_mac = parse(response, mac_pattern)
-            print ("Next Hop ARP MAC is: " + arp_mac)
-            response = ssh_login(hop_ip, mac_command, arp_mac)
-            arp_int = parse(response, po_int_pattern)
-            print ("Next Hop interface is: " + arp_int)
-            response = ssh_login(hop_ip, show_run_int_cmd, arp_int)
-            ssh_output = response.read()
-            raw_output = ssh_output.decode(encoding='utf-8')
-            print ('-----------------------------------------\n')
-            print ('Next Hop Interface Config: ')
-            print (raw_output)
-            print ('\n \n')
-            return 'static'
-        else:
-            print ('No Static \n')
-
-    elif route != "Not Found":
-        print ('-----------------------------------------')
-        print ("Found a best ucast next-hop: " + route)
-        print ('-----------------------------------------\n')
-        #extract just the IP from the route
-        match = ip_only_pattern.search(route)
-        if match:
-            next_hop_ip = match.group(0)
-            print ("Next Hop IP is: " + next_hop_ip)
-            print ("\n \n")
-            return next_hop_ip
-        else:
-            print ('No Route \n \n')
-
-    elif connected != 'Not Found':
-        return 'connected'
-
-    else:
-        print ("Next-hop Error. Exiting. \n \n")
-        return
-
-def ssh_login(switch_ip, command, command_param):
-    ssh_client.connect(hostname=switch_ip,
-                       username=username,
-                       password=password)
-    stdin, stdout, stderr = ssh_client.exec_command(command + command_param)
-    return stdout
-
-def parse(response, pattern):
-    try:
-        ssh_output = response.read()
-        raw_output = ssh_output.decode(encoding='utf-8')
-    except:
-        raw_output = response
-    match = pattern.search(raw_output)
-#    return matched_value
-    if match:
-        matched_value = match.group(0)
-        return matched_value
-    else:
-        return 'Not Found'
-
-def parse_cdp(response, pattern):
-    ssh_output = response.read()
-    raw_output = ssh_output.decode(encoding='utf-8')
-    match = pattern.search(raw_output)
-    global cdp_neighbors
-    if match:
-        phone_match = phone_pattern.search(raw_output)
-        if phone_match:
-            cdp_neighbors = cdp_neighbors + 1
-        else:
-            matched_value = match.group(1)
-            return matched_value
-    else:
-        cdp_neighbors = cdp_neighbors + 1
-        return
-
 # =========================== Main =================================
 
+
+print ('######################################################')
+print ('##### Welcome to the VUMC NEO - IP Trace Program #####')
+print ('#####                                            #####')
+print ('##### This program will find information about   #####')
+print ('##### where the device associated with an IP     #####')
+print ('##### is, or was last connected to the network.  ##### ')
+print ('###################################################### \n')
+
+# Gather user data
+print ('Enter the following information for use with all searches:')
+network_functions.get_creds()
+root_ip = '10.224.0.251'
+alt_root_ip = '10.224.0.252'
+
+while True:
+    ip = input('What is the IP you want to trace? ')
+    ip_type = network_functions.vumc_ip_type(ip)
+    if ip_type == 1:
+        ip_info = network_functions.getWirelessIpInfo(ip)
+        print(ip_info)
+    elif ip_type == 5:
+        print(f'{ip} is not a VUMC address and cannot be traced')
+    else:
+        gateway_info = network_functions.find_gateway(ip, root_ip)
+        if gateway_info:
+            trace = network_functions.mac_trace(gateway_info, ip)
+        if not gateway_info:
+            print (f'Gateway IP Not Found. \n')
+            retry = input('Would you like to trace another IP? <Y or N>:')
+            if retry == 'y' or retry == 'Y':
+                continue
+            else:
+                exit()
+    name = input('Would you like to trace another IP? <Y or N>:')
+    if name == 'y' or name == 'Y':
+        continue
+    else:
+        exit()
+
+
+
+
+
+
+
+
+
+'''
 print ('######################################################')
 print ('##### Welcome to the VUMC NEO - IP Trace Program #####')
 print ('#####                                            #####')
@@ -216,39 +116,7 @@ while True:
             exit()
 
     # Print updates about gateway IP
-    print(f'The gateway is {gateway_ip}')
-    print(f'Logging into the local router for ' + ip_trace + ': ' + gateway_ip)
-    print('\n------------------------------------------------------------')
 
-    stdout = ssh_login(gateway_ip, arp_command, ip_trace)
-    mac_address = parse(stdout, mac_pattern)
-    print(f"The mac address associated with {ip_trace} is: " + mac_address)
-
-    stdout = ssh_login(gateway_ip, mac_command, mac_address)
-    interface = parse(stdout, int_pattern)
-    print(f"The associated interface with {mac_address} is: " + interface)
-
-    cdp_ip = gateway_ip
-
-    while cdp_neighbors == 0:
-        cdp_command = (f'show cdp neighbors ')
-        cdp_param = (f'{interface} detail')
-        stdout = ssh_login(cdp_ip, cdp_command, cdp_param)
-        cdp_ip = parse_cdp(stdout, ip_pattern)
-        if cdp_neighbors == 0:
-            print(f"The switch IP address associated with {interface} is: " + cdp_ip)
-            stdout = ssh_login(cdp_ip, mac_command, mac_address)
-            interface = parse_cdp(stdout, int_pattern)
-            print(f"The associated interface with {mac_address} is: " + interface)
-            last_switch_ip = cdp_ip
-        else:
-            print(f'The device is connected to switch {last_switch_ip} port {interface}')
-    stdout = ssh_login(last_switch_ip, snmp_command, '')
-    snmp_list = stdout.readlines()
-    snmp_loc = snmp_list[9]
-
-    # print out location result.
-    print(f"Location: {snmp_loc}")
 
     # code to loop the program until a user wants to exit.
     name = input('Would you like to trace another IP? <Y or N>:')
@@ -256,3 +124,4 @@ while True:
         continue
     else:
         exit()
+'''
--- network_functions.py
+++ network_functions.py
@@ -13,53 +13,38 @@ import sys
 import paramiko
 import pexpect
 from piapi import PIAPI
-import getpass
+from getpass import getpass
 from pprint import pprint
 
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+mac_pattern = re.compile('([0-9a-fA-F]{4}.[0-9a-fA-F]{4}.[0-9a-fA-F]{4})')
+int_pattern = re.compile('([a-zA-Z]{1,2}[0-9]{1}/[0-9]{1,2}/?[0-9]{1,2})')
+po_int_pattern = re.compile('([A-Z]{1}[a-z]{1}[0-9]{1,4})')
+ip_pattern = re.compile('IP address: ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})')
+subnet_pattern = re.compile('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2})')
+ip_only_pattern = re.compile('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})')
+ip_config_pattern = re.compile('ip address ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})')
+next_hop_pattern = re.compile('\*via ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})')
+static_route_pattern = re.compile('static')
+connected_pattern = re.compile('directly connected, via Vlan([0-9]{1,4})')
+phone_pattern = re.compile('(IP Phone)')
+show_run_int_cmd = 'show run interface '
+arp_command = 'show ip arp '
+mac_command = 'show mac address-table address '
+snmp_command = 'show snmp location'
+iproute_cmd = 'show ip route '
+cdp_neighbors = 0
+username = ''
+password = ''
 
-def getWirelessIpInfo(un, passw, ip_addr):
-    user = un
-    pw = passw
-    ipaddr = ip_addr
+def get_creds():
+    global username
+    global password
+    username = input('Username: ')
+    password = getpass('Password: ')
+    return
 
-    api = PIAPI("https://10.109.24.16/", user, pw, verify=False)
-
-    reply = api.request("ClientDetails", params={".full": "true", "ipAddress": ipaddr})
-    attached_ap = reply[0]['clientDetailsDTO']['apName']
-    ssid = reply[0]['clientDetailsDTO']['clientInterface']
-    location = reply[0]['clientDetailsDTO']['location']
-    ipInfo = "The device you searched for was last seen in " + location + " connected to " + attached_ap + " on the " + ssid + " network."
-    return ipInfo
-
-# takes a single IP input, runs a traceroute, and returns a list of every IP hop
-def traceroute(ip, max_hops=10, trace_timeout=500):
-    ip_pattern = re.compile('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})')
-    unreach_pattern = re.compile('Request timed out')
-    print(f'Tracing route to {ip}....')
-    trace_response = subprocess.check_output(f'tracert -h {max_hops} -w {trace_timeout} {ip}', shell=True)
-    raw_output = trace_response.decode(encoding='utf-8')
-    print(raw_output)
-    output_list = raw_output.split('\r\n')
-    del output_list[-3:-1]
-    match_unreach = unreach_pattern.search(output_list[-2])
-    if match_unreach:
-        print('Request timed out')
-        print('This IP address is not on the network and cannot be traced')
-    hop_list = []
-    for line in output_list:
-        match = ip_pattern.search(line)
-        if match:
-            matched_value = match.group(0)
-            hop_list.append(matched_value)
-        else:
-            continue
-    print('Hop List: \n')
-    pprint(hop_list)
-    return hop_list
-
-# function to validate whether an IP is a valid VUMC network IP, a public IP, or
-# a wireless IP and return the variable ip_type to be used later to determine
-# which path the program should take
 def vumc_ip_type(ip):
     global ip_type
     ip_type = 0
@@ -86,18 +71,258 @@ def vumc_ip_type(ip):
         ip_type += 5
         return ip_type
 
+def find_gateway(ip_trace, root_ip):
+    print(f'Attempting to trace route to {ip_trace}. Stand by.....')
+    hop_list = traceroute(ip_trace)
+    print(hop_list)
+    if hop_list != 'unreachable':
+        print(f'Contacting gateway device {hop_list[-2]}')
+        return hop_list[-2]
 
-#MAIN:
+    print ('Beginning ip route trace to locate gateway...')
 
-#hop_list = traceroute("10.100.60.8")
-#print (hop_list)
+    print ('Contacting device: ' + root_ip + ' to look for routes to ' + ip_trace + '...')
+    next_hop = get_next_hop(ip_trace, root_ip)
+    while True:
+        print ('contacting the next-hop: ' + next_hop)
+        hop_ip = next_hop
+        next_hop = get_next_hop(ip_trace, next_hop)
+        if next_hop == ip_only_pattern.search(next_hop):
+            continue
+        elif next_hop == 'connected':
+            response = ssh_login(hop_ip, iproute_cmd, ip_trace)
+            ssh_output = response.read()
+            raw_output = ssh_output.decode(encoding='utf-8')
+            connected = parse(raw_output, connected_pattern)
+            route = parse(raw_output, next_hop_pattern)
+            static = parse(raw_output, static_route_pattern)
 
-'''
-user = input("Username: ")
-pw = getpass.getpass('Password: ')
-ipaddr = input('IP Address: ')
+            match = connected_pattern.search(raw_output)
+            vlan_id = match.group(1)
+            svi = "vlan" + vlan_id
+            response = ssh_login(hop_ip, show_run_int_cmd, svi)
+            svi_ip_config = parse(response, ip_config_pattern)
+            gateway_ip = parse(svi_ip_config, ip_only_pattern)
+            print (gateway_ip)
+            return gateway_ip
 
-prime_resp = getWirelessIpInfo(user, pw, ipaddr)
+        else:
+            return
 
-print (prime_resp)
-'''
+# function to get log in to a network device and get the next hop for a route.
+def get_next_hop(ip_trace, hop_ip):
+    #log in to current hop and return show ip route output
+    print ('Looking for next hop to the Gateway IP for ' + ip_trace + ' .....')
+    response = ssh_login(hop_ip, iproute_cmd, ip_trace)
+    ssh_output = response.read()
+    raw_output = ssh_output.decode(encoding='utf-8')
+    connected = parse(raw_output, connected_pattern)
+    route = parse(raw_output, next_hop_pattern)
+    static = parse(raw_output, static_route_pattern)
+
+    print ('\n ')
+    print ('--------------------------')
+    print ('Route Results Summary: ')
+    print ('--------------------------\n')
+    print ('Static: ' + static)
+    print ('Route: ' + route)
+    print ('Connected: ' + connected)
+    print ('--------------------------\n')
+
+    if static != "Not Found":
+        print ('-----------------------------------------')
+        print ("Found a static next-hop: " + route + '\n')
+        print ('-----------------------------------------')
+        #extract just the IP from the route
+        match = ip_only_pattern.search(route)
+        if match:
+            next_hop_ip = match.group(0)
+            # gather arp information to identify use for outgoing interface identification
+            response = ssh_login(hop_ip, arp_command, next_hop_ip)
+            arp_mac = parse(response, mac_pattern)
+            print ("Next Hop ARP MAC is: " + arp_mac)
+            response = ssh_login(hop_ip, mac_command, arp_mac)
+            arp_int = parse(response, po_int_pattern)
+            print ("Next Hop interface is: " + arp_int)
+            response = ssh_login(hop_ip, show_run_int_cmd, arp_int)
+            ssh_output = response.read()
+            raw_output = ssh_output.decode(encoding='utf-8')
+            print ('-----------------------------------------\n')
+            print ('Next Hop Interface Config: ')
+            print (raw_output)
+            print ('\n \n')
+            return 'static'
+        else:
+            print ('No Static \n')
+
+    elif route != "Not Found":
+        print ('-----------------------------------------')
+        print ("Found a best ucast next-hop: " + route)
+        print ('-----------------------------------------\n')
+        #extract just the IP from the route
+        match = ip_only_pattern.search(route)
+        if match:
+            next_hop_ip = match.group(0)
+            print ("Next Hop IP is: " + next_hop_ip)
+            print ("\n \n")
+            return next_hop_ip
+        else:
+            print ('No Route \n \n')
+
+    elif connected != 'Not Found':
+        return 'connected'
+
+    else:
+        print ("Next-hop Error. Exiting. \n \n")
+        return
+
+def ssh_login(switch_ip, command, command_param):
+    ssh_client.connect(hostname=switch_ip,
+                       username=username,
+                       password=password)
+    stdin, stdout, stderr = ssh_client.exec_command(command + command_param)
+    return stdout
+
+def parse(response, pattern):
+    try:
+        ssh_output = response.read()
+        raw_output = ssh_output.decode(encoding='utf-8')
+    except:
+        raw_output = response
+    match = pattern.search(raw_output)
+#    return matched_value
+    if match:
+        matched_value = match.group(0)
+        return matched_value
+    else:
+        return 'Not Found'
+
+def parse_cdp(response, pattern):
+    ssh_output = response.read()
+    raw_output = ssh_output.decode(encoding='utf-8')
+    match = pattern.search(raw_output)
+    global cdp_neighbors
+    if match:
+        phone_match = phone_pattern.search(raw_output)
+        if phone_match:
+            cdp_neighbors = cdp_neighbors + 1
+        else:
+            matched_value = match.group(1)
+            return matched_value
+    else:
+        cdp_neighbors = cdp_neighbors + 1
+        return
+
+def getWirelessIpInfo(ipaddr):
+    user = username
+    pw = password
+    print(user)
+    api = PIAPI("https://10.109.24.16/", user, pw, verify=False)
+
+    reply = api.request("ClientDetails", params={".full": "true", "ipAddress": ipaddr})
+    attached_ap = reply[0]['clientDetailsDTO']['apName']
+    ssid = reply[0]['clientDetailsDTO']['clientInterface']
+    location = reply[0]['clientDetailsDTO']['location']
+    ipInfo = "The device you searched for was last seen in " + location + " connected to " + attached_ap + " on the " + ssid + " network."
+    return ipInfo
+
+# takes a single IP input, runs a traceroute, and returns a list of every IP hop
+def traceroute(ip, max_hops=10, trace_timeout=500):
+    ip_pattern = re.compile('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})')
+    unreach_pattern = re.compile('Request timed out')
+    print(f'Tracing route to {ip}....')
+    trace_response = subprocess.check_output(f'tracert -h {max_hops} -w {trace_timeout} {ip}', shell=True)
+    raw_output = trace_response.decode(encoding='utf-8')
+    print(raw_output)
+    output_list = raw_output.split('\r\n')
+    del output_list[-3:-1]
+    match_unreach = unreach_pattern.search(output_list[-2])
+    if match_unreach:
+        print('Request timed out')
+        print('This IP address is not on the network and cannot be traced')
+        return 'unreachable'
+    hop_list = []
+    for line in output_list:
+        match = ip_pattern.search(line)
+        if match:
+            matched_value = match.group(0)
+            hop_list.append(matched_value)
+        else:
+            continue
+    print('Hop List: \n')
+    pprint(hop_list)
+    return hop_list
+
+# function to validate whether an IP is a valid VUMC network IP, a public IP, or
+# a wireless IP and return the variable ip_type to be used later to determine
+# which path the program should take
+def vumc_ip_type(ip):
+    global ip_type
+    ip_type = 0
+    octet_list = ip.split('.')
+    if octet_list[0] == '10':
+        if octet_list[1] == '248' or octet_list[1] == '249' or octet_list[1] == '250' or octet_list[1] == '251':
+            print(f'{ip} is a VUMC Wireless IP')
+            ip_type = 1
+            return ip_type
+        else:
+            print(f'{ip} is a VUMC wired IP')
+            ip_type = 2
+            return ip_type
+    elif octet_list[0] == '160' and octet_list[1] == '129':
+        print(f'{ip} is a VUMC public IP address')
+        ip_type = 3
+        return ip_type
+    elif octet_list[0] == '129' and octet_list[1] == '59':
+        print(f'{ip} is a VU public IP address')
+        ip_type = 4
+        return ip_type
+    else:
+        print(f'{ip} is not a VUMC IP address')
+        ip_type = 5
+        return ip_type
+
+def mac_trace(gateway_ip, ip_trace):
+    mac_command = 'show mac address-table address '
+    arp_command = 'show ip arp '
+    snmp_command = 'show snmp location'
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    mac_pattern = re.compile('([0-9a-fA-F]{4}.[0-9a-fA-F]{4}.[0-9a-fA-F]{4})')
+    int_pattern = re.compile('([a-zA-Z]{1,2}[0-9]{1}/[0-9]{1,2}/?[0-9]{1,2})')
+    ip_pattern = re.compile('IP address: ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})')
+
+    print(f'The gateway is {gateway_ip}')
+    print(f'Logging into the local router for ' + ip_trace + ': ' + gateway_ip)
+    print('\n------------------------------------------------------------')
+
+    stdout = ssh_login(gateway_ip, arp_command, ip_trace)
+    mac_address = parse(stdout, mac_pattern)
+    print(f"The mac address associated with {ip_trace} is: " + mac_address)
+
+    stdout = ssh_login(gateway_ip, mac_command, mac_address)
+    interface = parse(stdout, int_pattern)
+    print(f"The associated interface with {mac_address} is: " + interface)
+
+    cdp_ip = gateway_ip
+
+    while cdp_neighbors == 0:
+        cdp_command = (f'show cdp neighbors ')
+        cdp_param = (f'{interface} detail')
+        stdout = ssh_login(cdp_ip, cdp_command, cdp_param)
+        cdp_ip = parse_cdp(stdout, ip_pattern)
+        if cdp_neighbors == 0:
+            print(f"The switch IP address associated with {interface} is: " + cdp_ip)
+            stdout = ssh_login(cdp_ip, mac_command, mac_address)
+            interface = parse_cdp(stdout, int_pattern)
+            print(f"The associated interface with {mac_address} is: " + interface)
+            last_switch_ip = cdp_ip
+        else:
+            print(f'The device is connected to switch {last_switch_ip} port {interface}')
+    stdout = ssh_login(last_switch_ip, snmp_command, '')
+    snmp_list = stdout.readlines()
+    snmp_loc = snmp_list[9]
+
+    # print out location result.
+    print(f"Location: {snmp_loc}")
+    return snmp_loc
